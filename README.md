b5b4b393f232030dee6e53526bd3138e23e59993
Vinicius Zaccani <vinicius.zaccani@gmail.com>
2025-01-17 20:04:27 -0300

    segunda parte dos exercicios

---
+++ README.md
@@ -0,0 +1,1 @@
+Lista de exercicios para processo seletivo da empresa
---
+++ ex5.js
@@ -0,0 +1,23 @@
+let string = "inverter a string"
+let invertido = ""
+
+for (let i = (string.length - 1); i>=0; i--){
+    invertido += string[i];
+}
+
+console.log(`resultado: \n${string} \n${invertido}`)
+
+
+
+
+
+
+/*
+
+5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse;
+
+*/
